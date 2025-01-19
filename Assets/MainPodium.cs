55c1cb0f1027028d6a76c120596e406d85c7f677
actually-reb <61338113+actually-reb@users.noreply.github.com>
2025-01-18 22:44:29 -0800

    added basic podium spin

---
+++ Assets/MainPodium.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainPodium : MonoBehaviour
+{
+    [SerializeField]
+	Transform rotationBase;
+    float rotation;
+    float deltaRotation;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotation = 0.0f;
+        deltaRotation = 5.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0)) {
+            deltaRotation = Input.GetAxis("Mouse X") * -1000;
+        } else {
+            deltaRotation = 0;
+        }
+        
+        rotation += deltaRotation * Time.deltaTime;
+        rotationBase.localRotation = Quaternion.Euler(0, rotation, 0);
+    }
+}

5e7f282c3b9ee77503560c8c63c893d4e56d7297
SageTafoya <tafoyas@oregonstate.edu>
2025-01-19 17:25:55 -0800

    got augmented reality to work with test space

---
+++ Assets/rotateCylindar.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateCylindar : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.Rotate(0, 100.0f*Time.deltaTime, 0);
+    }
+}

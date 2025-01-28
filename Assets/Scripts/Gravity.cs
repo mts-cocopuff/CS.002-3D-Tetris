b6013c55c5caf7754478e806aab0b9eb1a8e7589
Alex Giger <57809929+giiger@users.noreply.github.com>
2025-01-27 17:20:42 -0800

    Created centrifugal force and rotation for turntable

--- Assets/Scripts/Gravity.cs
+++ Assets/Scripts/Gravity.cs
@@ -2,14 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Gravity : MonoBehaviour
 {
+
+    public GameObject cylinder;
+    private bool isColliding = false;
+    Turntable turntableScript;
+
+
+    void Start()
+    {
+        turntableScript = cylinder.GetComponent<Turntable>();
+    }
 
     void OnCollisionEnter(Collision collision){
         //set rigidbody usegravity
         GetComponent<Rigidbody>().useGravity = true;
-        this.enabled = false; // Disables the script on collision
-
+        isColliding = true;
     }
     public float speed;
 
@@ -17,7 +27,12 @@ public class Gravity : MonoBehaviour
     void Update()
     {
         //while rigidbody is not colliding with anything, move down
-        
-        transform.Translate(Vector3.down * speed * Time.deltaTime);
+        if (!isColliding)
+        {
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+        }
+        else{
+            
+        }
     }
 }

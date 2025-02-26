75cec73b545f4b7775accd0b12ca8e90a51d6993
Alex Giger <57809929+giiger@users.noreply.github.com>
2025-02-26 15:20:38 -0800

    Added game over
    for if player loses due to topout

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,6 +6,8 @@ public class Spawner : MonoBehaviour
 {
     public GameObject[] tetrisPieces;
     public Transform spawnPoint;
+    private float timeSinceLastSpawn;
+    private float lastSpawnTime = -1f;
 
     private void Start()
     {
@@ -14,9 +16,20 @@ public class Spawner : MonoBehaviour
 
     public void SpawnRandomPiece()
     {
+        timeSinceLastSpawn = Time.time - lastSpawnTime;
+
+        if (timeSinceLastSpawn < 0.2f)
+        {
+            Debug.Log("GAME OVER. PIECES TOP OUT");
+            this.enabled = false;
+            return;
+        }
+
         if (tetrisPieces.Length == 0) return;
 
         int randomIndex = Random.Range(0, tetrisPieces.Length);
         Instantiate(tetrisPieces[randomIndex], spawnPoint.position, Quaternion.identity);
+
+        lastSpawnTime = Time.time;
     }
 }

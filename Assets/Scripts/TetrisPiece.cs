234e9d0232ea96d3b0767053eaf0201216a0db2d
yeoki-osu <102574539+yeoki-osu@users.noreply.github.com>
2025-03-02 20:26:22 -0800

    Integrate QuickOutline for highlighting current playable piece

--- Assets/Scripts/TetrisPiece.cs
+++ Assets/Scripts/TetrisPiece.cs
@@ -8,11 +8,18 @@ public class TetrisPiece : MonoBehaviour
     private Transform baseContainer = null;
     private bool setPiece = false;
     private float despawnHeight = -5f;
+
+    private Outline outline = null;
+
+    [SerializeField] private Color outlineColor = Color.white;
+    [SerializeField, Range(0f, 10f)] private float outlineWidth = 5f;
     
     void Start()
     {
         // spawner = FindObjectOfType<Spawner>();
         baseContainer = GameObject.Find("BaseContainer")?.transform;
+
+        ApplyOutline();
     }
 
     private void Update()
@@ -30,8 +37,23 @@ public class TetrisPiece : MonoBehaviour
             if (!setPiece){
                 spawner.SpawnRandomPiece();
                 transform.SetParent(baseContainer, true);
+                RemoveOutline();
                 setPiece = true;
             }
         }
+    }
+
+    private void ApplyOutline()
+    {
+        outline = gameObject.AddComponent<Outline>();
+        outline.OutlineMode = Outline.Mode.OutlineAll;
+        outline.OutlineColor = outlineColor;
+        outline.OutlineWidth = outlineWidth;
+        outline.enabled = true;
+    }
+
+    private void RemoveOutline()
+    {
+        outline.enabled = false;
     }
 }

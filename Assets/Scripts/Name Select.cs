aff1b7ffa7645fee65cce44579312a9a0bed0c90
tafoyas <tafoyas@oregonstate.edu>
2025-05-05 23:14:00 -0700

    working on name select

---
+++ Assets/Scripts/Name Select.cs
@@ -0,0 +1,95 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using TMPro;
+// using UnityEngine.UI;
+
+// public class NameSelect : MonoBehaviour
+// {
+//     // Start is called before the first frame update
+
+//     public int NAMELETTERS N1=65, N2=65, N3=65;
+
+//     public TextMeshProUGUI letter1;
+//     public TextMeshProUGUI letter2;
+//     public TextMeshProUGUI letter3;
+
+//     public Button button1;
+//     public Text test1;
+
+//     GameObject myObject = GameObject.Find("UP SELCT");
+//     void Start()
+//     {
+        
+    
+
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         //update button 1
+
+//         //update button 2
+
+//         //update button 3
+//     }
+
+
+//     public void increaseN1()
+//     {
+//         N1++;
+//         if (N1 > 90)
+//         {
+//             N1 = 65;
+//         }
+//     }
+
+//     public void decreaseN1()
+//     {
+//         N1--;
+//         if (N1 < 65)
+//         {
+//             N1 = 90;
+//         }
+//     }
+    
+//     public void increaseN2()
+//     {
+//         N2++;
+//         if (N2 > 90)
+//         {
+//             N2 = 65;
+//         }
+//     }
+
+//     public void decreaseN2()
+//     {
+//         N2--;
+//         if (N2 < 65)
+//         {
+//             N2 = 90;
+//         }
+//     }
+
+//     public void increaseN3()
+//     {
+//         N3++;
+//         if (N3 > 90)
+//         {
+//             N3 = 65;
+//         }
+//     }
+
+//     public void decreaseN3()
+//     {
+//         N3--;
+//         if (N3 < 65)
+//         {
+//             N3 = 90;
+//         }
+//     }
+
+
+
+// }

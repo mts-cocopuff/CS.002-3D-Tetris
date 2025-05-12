8e4ec34474cc34c644fd8d43e765cbe41aa5c154
SageTafoya <tafoyas@oregonstate.edu>
2025-05-12 13:35:29 -0700

    Piece and area rotaion working, Walls seperated from base object, Friction not working

--- Assets/Scripts/ControllerRotation.cs
+++ Assets/Scripts/ControllerRotation.cs
@@ -8,14 +8,14 @@ using TMPro;
 
 // public class ControllerRotation : MonoBehaviour
 // {
-//     public Transform targetObject; // The object that will rotate
+//     // public Transform targetObject; // The object that will rotate
 
 //     public Rigidbody rb; // The rigidbody of the object
 
 
 //     void Update()
 //     {
-//         if (targetObject == null) return; // Prevent errors if no object is assigned
+//         // if (targetObject == null) return; // Prevent errors if no object is assigned
 
 //         // Get the Left Hand controller
 //         UnityEngine.XR.InputDevice leftController = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
@@ -27,12 +27,12 @@ using TMPro;
 //             Vector3 controllerEuler = controllerRotation.eulerAngles;
 
 //             // Extract only the Y-axis rotation (this allows us apply this rotation value to the turn table)
-//             Vector3 targetEuler = targetObject.eulerAngles;
+//             // Vector3 targetEuler = targetObject.eulerAngles;
 //             Vector3 newEuler = rb.rotation.eulerAngles;
 //             targetEuler.y = controllerEuler.y; // Apply only Y rotation
 
 //             // Apply the new rotation to the target object (the turn table)
-//             targetObject.rotation = Quaternion.Euler(targetEuler);
+//             // targetObject.rotation = Quaternion.Euler(targetEuler);
 //             Quaternion newRotation = Quaternion.Euler(targetEuler);
 //             rb.MoveRotation(newRotation); 
 //         }
@@ -43,7 +43,11 @@ public class ControllerRotation : MonoBehaviour
 {
 
     public Rigidbody rb; // The rigidbody of the object
+    public Rigidbody rb2; // The rigidbody of the object
 
+    public float oldrotation;
+    public float newrotation;
+    
     void Start()
     {
         // Get the Rigidbody component attached to this GameObject
@@ -71,12 +75,22 @@ public class ControllerRotation : MonoBehaviour
             // Extract only the Y-axis rotation (this allows us apply this rotation value to the turn table)
             // Vector3 targetEuler = targetObject.eulerAngles;
             Vector3 targetEuler = rb.rotation.eulerAngles;
+
             targetEuler.y = controllerEuler.y; // Apply only Y rotation
+            // oldrotation = targetEuler.y;
+
 
             // Apply the new rotation to the target object (the turn table)
             Quaternion newRotation = Quaternion.Euler(targetEuler);
+
             rb.MoveRotation(newRotation); 
+
+
+            // float turn = Input.GetAxis("Horizontal");
+            // rb.AddTorque(transform.up * turn, ForceMode.VelocityChange);
         }
+
+
 
 
     }
